5db12f94e574d57e891f975b5ef2eca13a1b858d
gadjodilo83 <gadjodilo1983@gmail.com>
2023-07-09 14:46:13 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,44 +28,51 @@ const Home = () => {
     },
   });
 
-  const fetchGpt = async () => {
-    const userPrompt = window.transcript;
-	const systemPrompt = translationDirection === "de-it"
-	  ? `
-		  Du bist ein Sprachlehrer und übersetzt jeden Input direkt ins Deutsche und Italienische. 
-		  Du gibst auch Vorschläge, wie auf Fragen geantwortet werden kann oder wie das Gespräch fortgesetzt werden könnte, jeweils auf Deutsch und Italienisch.
-		`
-	  : `
-		  Sei un insegnante di lingue e traduci ogni input direttamente in tedesco e italiano.
-		  Fornisci anche suggerimenti su come rispondere a una domanda o come proseguire la conversazione, sia in tedesco che in italiano.
-		`;
+const fetchGpt = async () => {
+  const userPrompt = window.transcript;
+  const systemPrompt = translationDirection === "de-it"
+    ? `
+        Du bist ein Sprachlehrer und übersetzt jeden Input direkt ins Deutsche und Italienische. 
+        Du gibst auch Vorschläge, wie auf Fragen geantwortet werden kann oder wie das Gespräch fortgesetzt werden könnte, jeweils auf Deutsch und Italienisch.
+      `
+    : `
+        Sei un insegnante di lingue e traduci ogni input direttamente in tedesco e italiano.
+        Fornisci anche suggerimenti su come rispondere a una domanda o come proseguire la conversazione, sia in tedesco che in italiano.
+      `;
 
-	const response = await fetch(`https://api.openai.com/v1/completions`, {
-	  body: JSON.stringify({
-		model: "gpt-3.5-turbo",
+  const response = await fetch(`https://api.openai.com/v1/engines/davinci-codex/completions`, {
+    body: JSON.stringify({
+      prompt:
+        systemPrompt +
+        "\n\nUser Prompt: " +
+        userPrompt +
+        "\n\nDeutsche Übersetzung:\n" +
+        'Antwortvorschlag auf Deutsch: "' +
+        userPrompt +
+        ' auf Deutsch"\n\nItalienische Übersetzung:\n' +
+        'Antwortvorschlag auf Italienisch: "' +
+        userPrompt +
+        ' auf Italienisch"\n\nVorschlag für Gesprächsweiterführung:\n' +
+        'Vorschlag auf Deutsch: "' +
+        userPrompt +
+        ' auf Deutsch"\n\nVorschlag auf Italienisch: "' +
+        userPrompt +
+        ' auf Italienisch"',
+      max_tokens: 100,
+      temperature: 0.7,
+    }),
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
 
-        prompt:
-          systemPrompt +
-          "\ntranscript: " +
-          userPrompt +
-          "\nÜbersetze alles auf deutsch und italienisch: ",
-        temperature: 0.7,
-        max_tokens: 512,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      }),
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    });
-
-    const resJson = await response.json();
-    const res = resJson?.choices?.[0]?.text;
-    if (!res) return;
-    await displayRawRizz(res);
-  };
+  const resJson = await response.json();
+  const res = resJson?.choices?.[0]?.text;
+  if (!res) return;
+  await displayRawRizz(res);
+};
 
   useEffect(() => {
     const typingTimer = setInterval(() => {
