bd7dfd46eed2e864f9322893ef8c9ffa2eab3e3f
gadjodilo83 <gadjodilo1983@gmail.com>
2023-07-09 11:37:16 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,121 +1,114 @@
 import Head from "next/head";
+import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useState, useEffect } from "react";
 import { ensureConnected } from "@/utils/bluetooth/js/main";
 import { replRawMode, replSend } from "@/utils/bluetooth/js/repl";
 import { Button } from "antd";
 import { useWhisper } from "@chengsokdara/use-whisper";
 import { app } from "@/utils/app";
 import { execMonocle } from "@/utils/comms";
-import React, { useState, useEffect } from 'react';
 
-const swissFlag = '/swiss.png';
-const italianFlag = '/italy.png';
-
-// BatteryStatus component
-const BatteryStatus = () => {
-  const [batteryStatus, setBatteryStatus] = useState({});
-
-  useEffect(() => {
-    navigator.getBattery().then((battery) => {
-      const { level, charging } = battery;
-      setBatteryStatus({ level: level * 100, charging });
-
-      battery.addEventListener("levelchange", (event) => {
-        setBatteryStatus({ level: battery.level * 100, charging: battery.charging });
-      });
-
-      battery.addEventListener("chargingchange", (event) => {
-        setBatteryStatus({ level: battery.level * 100, charging: battery.charging });
-      });
-    });
-  }, []);
-
-  return (
-    <div className={styles['battery-status']} style={{ color: 'green', fontSize: '12px', backgroundColor: 'white' }}>
-      <p>{batteryStatus.level}%</p>
-    </div>
-  );
-};
+const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
-  const [activeButton, setActiveButton] = useState(null);
-  const [context, setContext] = useState('');
-  const [isRecording, setIsRecording] = useState(false);
   const [connected, setConnected] = useState(false);
-  const [currentFlag, setCurrentFlag] = useState(swissFlag);
-  const [inputLanguage, setInputLanguage] = useState('de');
-  const [temperature, setTemperature] = useState(0.9);
-  const [systemPrompt, setSystemPrompt] = useState('');
-  const [chatGptResponse, setChatGptResponse] = useState('');
-  const [typingIndex, setTypingIndex] = useState(0);
-  const [apiKey, setApiKey] = useState(process.env.NEXT_PUBLIC_OPENAI_API_TOKEN); // add this line
+  const [isRecording, setIsRecording] = useState(false);
 
-	const { startRecording, stopRecording, transcript } = useWhisper({
-	  apiKey: apiKey,
-	  streaming: true,
-	  timeSlice: 500,
-	  whisperConfig: {
-		language: inputLanguage,
-	  },
-	});
-
-const fetchGpt = async () => {
-  const userPrompt = window.transcript;
-  let promptContext = context;
-
-  if (activeButton === "A") {
-    promptContext = "Vordefinierter Text für Button A";
-  } else if (activeButton === "B") {
-    promptContext = "Vordefinierter Text für Button B";
-  }
-
-  const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      temperature: 0.9,
-      max_tokens: 2000,
-      messages: [
-        {"role": "system", "content": "Du bist ein Sprachübersetzer. Übersetze jeden Eingabetext sofort auf Deutsch und Italienisch, auch wenn es eine Frage ist. Mache anschliessend einen Antwortvorschlag oder einen Vorschlag um die Konversation weiterzuführen auf deutsch und italienisch" + promptContext},
-        {"role": "user", "content": "Übersetze den Eingabetext mache anschliessend einen Antwortvorschlag oder einen Vorschlag um die Konversation weiterzuführen auf deutsch und italienisch: " + userPrompt}
-      ]
-    }),
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-      "Content-Type": "application/json",
+  const { startRecording, stopRecording, transcript } = useWhisper({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_TOKEN,
+    streaming: true,
+    timeSlice: 500,
+    whisperConfig: {
+      language: "de",
     },
-    method: "POST",
   });
 
-  const resJson = await response.json();
-  const res = resJson?.choices?.[0]?.message?.content;
-  if (!res) return;
-  return res;
-};
+  const fetchGpt = async () => {
+    const userPrompt = window.transcript;
+    const systemPrompt = `
+
+		Du bist ein Übersetzer und übersetzte jeden Input direkt in Deutsch und auf Italienisch. 
+		Du machst auch Vorschläge, 
+		um auf eine Frage eine Antwort zu geben oder wie man das Gespräch weiterführen könnte und dies jeweil immer auf deutsch und italienisch.
+
+    `;
+    const response = await fetch(`https://api.openai.com/v1/completions`, {
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt:
+          systemPrompt +
+          "\ntranscript: " +
+          userPrompt +
+          "\nÜbersetzung auf deutsch und italienisch: ",
+        temperature: 0.7,
+        max_tokens: 512,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      }),
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+    const resJson = await response.json();
+    const res = resJson?.choices?.[0]?.text;
+    if (!res) return;
+    await displayRawRizz(res);
+  };
 
   useEffect(() => {
-    if (activeButton === 'A') {
-      setContext(`Du bist ein Sprachübersetzer. Übersetze jeden Eingabetext sofort auf Deutsch und Italienisch, auch wenn es eine Frage ist. Mache anschliessend einen Antwortvorschlag oder einen Vorschlag um die Konversation weiterzuführen auf deutsch und italienisch`);
-    } else if (activeButton === 'B') {
-      setContext(`Sei un traduttore di lingue. Traduci istantaneamente qualsiasi testo di input in tedesco e italiano, anche se si tratta di una domanda. Quindi dare un suggerimento di risposta o un suggerimento per continuare la conversazione in tedesco e italiano`);
-    }
-  }, [activeButton]);
+    // Sync the window variable and the transcript
+    window.transcript = transcript.text;
+  }, [transcript.text]);
+
+  return (
+    <>
+      <Head>
+        <title>rizzGPT</title>
+        <meta name="description" content="Generated by create next app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${inter.className} ${styles.main}`}>
+        <div className="flex w-screen h-screen flex-col items-center justify-center">
+          <p className="text-3xl">{connected ? "Connected" : "Disconnected"}</p>
+          {transcript.text}
+          <Button
+            type="primary"
+            onClick={async () => {
+              await ensureConnected(logger, relayCallback);
+              app.run(execMonocle);
+              await displayRawRizz();
+            }}
+          >
+            Connect
+          </Button>
+          <div className="flex items-center mt-5 gap-2">
+            <Button onClick={onRecord}>
+              {isRecording ? "Stop recording" : "Start recording"}
+            </Button>
+            <Button onClick={fetchGpt}>Get response</Button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 
   function relayCallback(msg) {
     if (!msg) {
       return;
     }
     if (msg.trim() === "trigger b") {
-      setInputLanguage("it");
-      setCurrentFlag(italianFlag);
-      setSystemPrompt(/* ... */);
-      setContext(`Sei un traduttore di lingue. Traduci istantaneamente qualsiasi testo di input in tedesco e italiano, anche se si tratta di una domanda. Quindi dare un suggerimento di risposta o un suggerimento per continuare la conversazione in tedesco e italiano`);
+      // Left btn
+      // fetchGpt();
     }
 
     if (msg.trim() === "trigger a") {
-      setInputLanguage("de");
-      setCurrentFlag(swissFlag);
-      setSystemPrompt(/* ... */);
-      setContext(`Du bist ein Sprachübersetzer. Übersetze jeden Eingabetext sofort auf Deutsch und Italienisch, auch wenn es eine Frage ist. Mache anschliessend einen Antwortvorschlag oder einen Vorschlag um die Konversation weiterzuführen auf deutsch und italienisch`);
+      // Right btn
+      // onRecord();
     }
   }
 
@@ -136,25 +129,21 @@ const fetchGpt = async () => {
     return text;
   }
 
-async function displayRizz(rizz) {
-  if (!rizz) return;
-  const splitText = wrapText(rizz);
-  let replCmd = "import display;";
+  async function displayRizz(rizz) {
+    if (!rizz) return;
+    const splitText = wrapText(rizz);
+    let replCmd = "import display;";
 
-  for (let i = 0; i < splitText.length; i++) {
-    const text = splitText[i];
-    for (let j = 0; j < text.length; j++) {
-      const delay = j * 100; // Zeitverzögerung pro Buchstabe (hier: 100ms)
-      replCmd += `timer.delay(${delay});`;
-      replCmd += `display.text("${text.substring(0, j + 1)}", 0, ${i * 50}, 0xffffff);`;
-      replCmd += "display.show();";
+    for (let i = 0; i < splitText.length; i++) {
+      replCmd += `display.text("${splitText[i]}", 0, ${i * 50}, 0xffffff);`;
     }
+
+    replCmd += "display.show();";
+
+    console.log("**** replCmd ****", replCmd);
+
+    await replSend(replCmd);
   }
-
-  console.log("**** replCmd ****", replCmd);
-
-  await replSend(replCmd);
-}
 
   async function displayRawRizz(rizz) {
     await replRawMode(true);
@@ -166,105 +155,6 @@ async function displayRizz(rizz) {
       setConnected(true);
     }
   }
-
-  useEffect(() => {
-    const typingTimer = setInterval(() => {
-      if (typingIndex < chatGptResponse.length) {
-        setTypingIndex(typingIndex + 1);
-      }
-    }, 50);
-
-    return () => clearInterval(typingTimer);
-  }, [chatGptResponse, typingIndex]);
-
-  return (
-    <>
-      <Head>
-        <title>switGPT</title>
-        <meta name="description" content="Generated by create Marc Simon Frei" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
-      </Head>
-
-      <main className="flex w-screen h-screen flex-col items-center justify-center">
-        <p
-          className="text-3xl mb-4"
-          style={{ color: connected ? 'green' : 'red' }}
-        >
-          {connected ? "Monocle Connected" : "Monocle Disconnected"}
-        </p>
-
-        <div className="mb-4">
-          <img src={currentFlag} alt="Current language flag" />
-        </div>
-
-        <Button
-          type="primary"
-          onClick={async () => {
-            await ensureConnected(logger, relayCallback);
-            app.run(execMonocle);
-            const res = await fetchGpt();
-
-            if (res) {
-              setChatGptResponse(res);
-              await displayRawRizz(res);
-            }
-          }}
-        >
-          Connect
-        </Button>
-
-        <div className="mt-2 flex gap-2">
-          <Button onClick={() => relayCallback("trigger a")}>
-            Switch to German
-          </Button>
-          <Button onClick={() => relayCallback("trigger b")}>
-            Switch to Italian
-          </Button>
-        </div>
-
-        <div className="flex flex-col items-center mt-5 gap-2">
-          <Button onClick={onRecord}>
-            {isRecording ? "Stop recording" : "Start recording"}
-          </Button>
-          <BatteryStatus />
-        </div>
-
-        <div>
-          <label>
-            Kontext:
-            <textarea style={{ width: "800px", height: "200px" }} value={context} onChange={(e) => setContext(e.target.value)} />
-          </label>
-        </div>
-
-        <div>
-          <label>
-            Temperatur:
-            <input type="number" min="0" max="2" step="0.1" value={temperature} onChange={(e) => setTemperature(parseFloat(e.target.value))} />
-          </label>
-        </div>
-
-	<div>
-	  <label>
-		API Key:
-		<input type="text" value={apiKey.replace(/[<>]/g, '')} onChange={(e) => setApiKey(e.target.value)} />
-	  </label>
-	</div>
-
-        <div>
-          <label>
-            ChatGPT:
-            <textarea
-              style={{ width: "800px", height: "200px" }}
-              value={chatGptResponse.substring(0, typingIndex)}
-              readOnly
-            />
-          </label>
-        </div>
-      </main>
-    </>
-  );
 };
 
 export default Home;
