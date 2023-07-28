46fe443c54d18faff72d8f43e25fb82112d7d944
gadjodilo83 <gadjodilo1983@gmail.com>
2023-07-28 17:56:12 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -71,7 +71,7 @@ const Home = () => {
         model: "gpt-3.5-turbo",
         messages: messages,
         temperature: temperature,
-        max_tokens: 400,
+        max_tokens: 250,
       }),
       headers: {
         Authorization: `Bearer ${apiKey}`,
@@ -205,7 +205,7 @@ async function displayRizz(rizz) {
   if (!rizz) return;
 
   const splitText = wrapText(rizz);
-  const groupSize = 5;
+  const groupSize = 4;
 
   for (let i = 0; i < splitText.length; i += groupSize) {
     const group = splitText.slice(i, i + groupSize);
@@ -218,9 +218,9 @@ async function displayRizz(rizz) {
     const textCmd = `display.show([${textCmds.join(", ")}])`;
     const clearCmd = "display.clear()";
 
-    await delay(7000); // 2.5 Sekunden warten
+    await delay(1000); // 2.5 Sekunden warten
     await replSend(`${clearCmd}\n${textCmd}\n`); // clear() und display.show senden
-	await delay(1000); // 2.5 Sekunden warten
+	await delay(7000); // 2.5 Sekunden warten
 
   }
 }
