7a67f1d1515a066550b13f7269eccd5b252f003f
gadjodilo83 <gadjodilo1983@gmail.com>
2023-08-14 10:58:55 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,7 @@ const Home = () => {
 
   const [apiKey, setApiKey] = useState(process.env.NEXT_PUBLIC_OPENAI_API_TOKEN);
   const [inputLanguage, setInputLanguage] = useState("de");
+  const [isFirstStart, setIsFirstStart] = useState(true);
   const [connected, setConnected] = useState(false);
   const [isRecordingState, setIsRecordingState] = useState(false);
   const isRecording = useRef(isRecordingState);
@@ -30,7 +31,7 @@ const Home = () => {
   const { startRecording: whisperStartRecording, stopRecording: whisperStopRecording, transcript } = useWhisper({
     apiKey: apiKey,
     streaming: true,
-    timeSlice: 5000,
+    timeSlice: 6000,
     whisperConfig: {
       language: inputLanguage,
     },
@@ -38,40 +39,53 @@ const Home = () => {
 
 const startMyRecording = async () => {
   const textCmd = `display.Text('Start Record', 320, 200, display.RED, justify=display.MIDDLE_CENTER)`;
-  const lineCmd = `display.Line(175, 230, 465, 230, display.RED)`;
+  const lineCmd = `display.Line(105, 230, 535, 230, display.RED)`;
   const showCmd = `display.show([${textCmd}, ${lineCmd}])`;
   await replSend(`${textCmd}\n${lineCmd}\n${showCmd}\n`);
   whisperStartRecording();
   setIsRecording(true);
+  
+  // Neue Animation
+  let animationCounter = 0;
+  const animationInterval = setInterval(async () => {
+    animationCounter++;
+    let animationText;
+    switch(animationCounter) {
+ 	  case 1:
+        animationText = `display.Text('Listening [=     ]', 320, 200, display.RED, justify=display.MIDDLE_CENTER)`;
+        break;
+	  case 2:
+        animationText = `display.Text('Listening [==    ]', 320, 200, display.RED, justify=display.MIDDLE_CENTER)`;
+        break;
+      case 3:
+        animationText = `display.Text('Listening [===   ]', 320, 200, display.RED, justify=display.MIDDLE_CENTER)`;
+        break;
+      case 4:
+        animationText = `display.Text('Listening [====  ]', 320, 200, display.RED, justify=display.MIDDLE_CENTER)`;
+        break;
+      case 5:
+        animationText = `display.Text('Listening [===== ]', 320, 200, display.RED, justify=display.MIDDLE_CENTER)`;
+        break;
+      case 6:
+        animationText = `display.Text('Listening [======]', 320, 200, display.RED, justify=display.MIDDLE_CENTER)`;
+        clearInterval(animationInterval);  // Stoppt die Animation nach 3 Iterationen
+        break;
+
+
+    }
+    const showAnimationCmd = `display.show([${animationText}, ${lineCmd}])`;
+    await replSend(`${animationText}\n${showAnimationCmd}\n`);
+  }, 1000);  // Alle 1000ms (2 Sekunden) aktualisieren
+
   setTimeout(async () => {
-	await stopMyRecording(true); 
-	await showAutomaticStop();
-  }, 5000);  // 5000 milliseconds = 8 seconds
+    clearInterval(animationInterval);  // Stoppt die Animation, falls sie noch läuft
+	stopMyRecording();  // Stoppt die Aufnahme
+  }, 6000);  // 6000 milliseconds = 6 seconds
 }
-
-const showAutomaticStop = async () => {
-  const textCmd = `display.Text('Automatic Stop', 320, 200, display.BLUE, justify=display.MIDDLE_CENTER)`;
-  const lineCmd = `display.Line(175, 230, 465, 230, display.BLUE)`;
-  const showCmd = `display.show([${textCmd}, ${lineCmd}])`;
-  await replSend(`${textCmd}\n${lineCmd}\n${showCmd}\n`);
-  setTimeout(async () => {
-    await clearDisplay();
-  }, 1000);
-}
-
-const clearDisplay = async () => {
-  const clearCmd = "display.clear()";
-  await replSend(`${clearCmd}\n`);
-}
-
 
 
 
 	const stopMyRecording = async () => {
-	  const textCmd = `display.Text('Stop Record', 320, 200, display.GREEN, justify=display.MIDDLE_CENTER)`;
-	  const lineCmd = `display.Line(175, 230, 465, 230, display.GREEN)`;
-	  const showCmd = `display.show([${textCmd}, ${lineCmd}])`;
-	  await replSend(`${textCmd}\n${lineCmd}\n${showCmd}\n`);
 	  whisperStopRecording();
 	  setIsRecording(false);
 
@@ -79,10 +93,8 @@ const clearDisplay = async () => {
 	  setTimeout(async () => {
 		if (transcript.text) {
 		  await fetchGpt();
-		} else {
-		  console.log('No transcript available');
-		}
-	  }, 1000); // Wartezeit in Millisekunden
+		} 
+	  }, 100); // Wartezeit in Millisekunden
 	}
 
   const relayCallback = (msg) => {
@@ -91,7 +103,6 @@ const clearDisplay = async () => {
     }
     if (msg.trim() === "trigger b") {
       // Left btn
-      console.log("Button B pressed");
       fetchGpt();
     }
 
@@ -138,11 +149,9 @@ const clearDisplay = async () => {
 
   const fetchGpt = async () => {
     if (fetching) {
-      console.log("Fetch already in progress");
       return;
     }
     setFetching(true);
-    console.log("fetchGpt called");
 
     try {
       const messages = [
@@ -155,7 +164,7 @@ const clearDisplay = async () => {
           model: "gpt-3.5-turbo",
           messages: messages,
           temperature: temperature,
-          max_tokens: 300,
+          max_tokens: 350,
         }),
         headers: {
           Authorization: `Bearer ${apiKey}`,
@@ -198,7 +207,7 @@ const clearDisplay = async () => {
     setLanguagePrompt(language);
   }, [language]);
 
-  return (
+return (
     <>
       <Head>
         <title>monocleTranslator</title>
@@ -206,32 +215,37 @@ const clearDisplay = async () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${inter.className} ${styles.main}`}>
-        <div className="flex w-screen h-screen flex-col items-center justify-start">
-          <h1 className="text-3xl">monocleTranslator</h1>
-          <p className="text-3xl mb-4">
-            {connected ? "Monocle Connected" : "Monocle Disconnected"}
+      <main className={`${inter.className} ${styles.main}`} style={{ background: 'linear-gradient(160deg, #1a1a1a 60%, #0d0d0d 100%)' }}>
+		<div className="flex w-screen h-screen flex-col items-center justify-center" style={{ marginTop: '-16%' }}>
+          <h1 className="text-3xl text-gradient mb-4" style={{ background: 'linear-gradient(90deg, #3f87a6, #ebf8e1, #f69d3c)', backgroundClip: 'text', WebkitBackgroundClip: 'text', color: 'transparent' }}>monocleGPT</h1>
+          <p className="text-3xl mb-4 text-white">
+            {connected ? "Connected" : "Disconnected"}
           </p>
-          <div style={{ width: "90%" }}>
+          <div className="flex flex-col" style={{ width: "90%", background: 'rgba(255, 255, 255, 0.1)', padding: '10px', borderRadius: '10px' }}>
             <Input
-              className="mb-2"
-              style={{ height: "40px" }}
+              className="mb-2 futuristic-input"
+              style={{ height: "40px", background: 'rgba(0, 0, 0, 0.5)', color: 'white', border: 'none' }}
               value={apiKey}
               onChange={(e) => setApiKey(e.target.value)}
               placeholder="API Key"
             />
-            <InputNumber
-              className="mb-2"
-              style={{ width: "100%", height: "40px" }}
-              min={0}
-              max={2}
-              step={0.1}
-              value={temperature}
-              onChange={(value) => setTemperature(value)}
-            />
+			<InputNumber
+			  className="mb-2 custom-input"
+			  style={{ 
+				width: "100%", 
+				height: "40px", 
+				background: 'rgba(0, 0, 0, 0.5)', 
+				border: 'none' 
+			  }}
+			  min={0}
+			  max={2}
+			  step={0.1}
+			  value={temperature}
+			  onChange={(value) => setTemperature(value)}
+			/>
             <Select
-              className="mb-2"
-              style={{ width: "100%", height: "40px" }}
+              className="mb-2 futuristic-select"
+              style={{ width: "100%", height: "40px", background: 'rgba(0, 0, 0, 0.5)', color: 'white', border: 'none' }}
               value={language}
               onChange={handleLanguageChange}
             >
@@ -241,38 +255,61 @@ const clearDisplay = async () => {
             </Select>
             <Input.TextArea
               className="mb-2"
-              style={{ height: "100px" }}
+              style={{ height: "100px", background: 'rgba(0, 0, 0, 0.5)', color: 'white', border: 'none' }}
               value={systemPrompt}
               placeholder="Define the role of GPT-3"
               onChange={(e) => setSystemPrompt(e.target.value)}
-              autoSize={{ minRows: 2, maxRows: 10 }}
+              autoSize={{ minRows: 2, maxRows: 6 }}
             />
-            <Button
-              className="mb-2"
-              type="primary"
-              onClick={async () => {
-                await ensureConnected(logger, relayCallback);
-                app.run(execMonocle);
-                await displayRawRizz();
-              }}
-            >
-              Connect
-            </Button>
-            <Button className="mb-2" onClick={fetchGpt}>
-              Get response
-            </Button>
+			<Button
+				className="mb-2 futuristic-button"
+				type="primary"
+				style={{ 
+					background: 'linear-gradient(90deg, #3f87a6, #f69d3c)', 
+					border: 'none', 
+					color: 'white', 
+					width: '100%', 
+					fontSize: '1.5rem', 
+					height: '60px',
+					margin: '0 auto',
+					display: 'flex',           // Verwenden Sie Flexbox
+					alignItems: 'center',      // Zentriert den Inhalt vertikal
+					justifyContent: 'center'  // Zentriert den Inhalt horizontal
+				}} 
+				onClick={async () => {
+					await ensureConnected(logger, relayCallback);
+					app.run(execMonocle);
+					await displayRawRizz();
+				}}
+			>
+				Connect
+			</Button>
           </div>
-          {transcript.text}
+          <p className="mt-4 text-white">{transcript.text}</p>
         </div>
       </main>
     </>
-  );
+);
 
 
-  async function displayRawRizz(rizz) {
-    await replRawMode(true);
+
+async function displayWelcomeMessage() {
+    const welcomeText = `display.Text('monocleGPT', 320, 150, display.WHITE, justify=display.MIDDLE_CENTER)`;  // Position angepasst
+    const readyText = `display.Text('Press the Button', 320, 250, display.WHITE, justify=display.MIDDLE_CENTER)`;  // Position angepasst
+    const showCmd = `display.show([${welcomeText}, ${readyText}])`;
+    await replSend(`${showCmd}\n`);
+}
+
+
+
+async function displayRawRizz(rizz) {
+    // await replRawMode(true);
+    if (isFirstStart) {
+        await displayWelcomeMessage(); // Zeige den Begrüßungstext nur beim ersten Start
+        setIsFirstStart(false); // Setzen Sie den Zustand auf false, da es nicht mehr das erste Mal ist
+    }
     await displayRizz(rizz);
-  }
+}
 
 
 async function displayRizz(rizz) {
@@ -292,9 +329,7 @@ async function displayRizz(rizz) {
 
       const textCmd = `display.show([${textCmds.join(", ")}])`;
 
-      await delay(10); // 2.5 Sekunden warten
-      await replSend(`${clearCmd}\n`);
-	  await delay(10); // Warten Sie 100 Millisekunden
+      // await replSend(`${clearCmd}\n`);
 	  await replSend(`${textCmd}\n`);
       await delay(5000); // 2.5 Sekunden warten
       // await replSend(`${clearCmd}\n`);
@@ -321,55 +356,64 @@ async function displayRizz(rizz) {
   }
 
 
-
 function cleanText(inputText) {
-  let cleanedText = inputText.replace(/\\/g, ""); // remove backslashes
-  cleanedText = cleanedText.replace(/""/g, '"'); // replace double quotes with single quotes
-  cleanedText = cleanedText.replace(/\n/g, ""); // remove line breaks
-  cleanedText = cleanedText.replace(/ä/g, "ae"); // replace "ä" with "ae"
-  cleanedText = cleanedText.replace(/ü/g, "ue"); // replace "ü" with "ue"
-  cleanedText = cleanedText.replace(/ö/g, "oe"); // replace "ö" with "oe"
-  cleanedText = cleanedText.replace(/Ä/g, "Ae"); // replace "Ä" with "Ae"
-  cleanedText = cleanedText.replace(/Ü/g, "Ue"); // replace "Ü" with "Ue"
-  cleanedText = cleanedText.replace(/Ö/g, "Oe"); // replace "Ö" with "Oe"
-  cleanedText = cleanedText.replace(/ß/g, "ss"); // replace "ß" with "ss"
-  cleanedText = cleanedText.replace(/ù/g, "u"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/à/g, "a"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/À/g, "A"); // replace "ä" with "ae"
-  cleanedText = cleanedText.replace(/è/g, "e"); // replace "ü" with "ue"
-  cleanedText = cleanedText.replace(/É/g, "E"); // replace "ö" with "oe"
-  cleanedText = cleanedText.replace(/é/g, "e"); // replace "Ä" with "Ae"
-  cleanedText = cleanedText.replace(/È/g, "E"); // replace "Ü" with "Ue"
-  cleanedText = cleanedText.replace(/Ú/g, "U"); // replace "Ö" with "Oe"
-  cleanedText = cleanedText.replace(/Ù/g, "U"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/ó/g, "o"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/Ó/g, "O"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/ò/g, "o"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/Ò/g, "O"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/l'u/g, "l u"); // replace "Ä" with "Ae"
-  cleanedText = cleanedText.replace(/l'a/g, "l a"); // replace "Ü" with "Ue"
-  cleanedText = cleanedText.replace(/dall'/g, "dall "); // replace "Ö" with "Oe"
-  cleanedText = cleanedText.replace(/dell'/g, "dell "); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/all'/g, "all "); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/sull'/g, "sull "); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/nell'/g, "nell "); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/quell'/g, "quell "); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/un'a/g, "un a"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/un'u/g, "un u"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/un'o/g, "un o"); // replace "ù" with "u"  
-  cleanedText = cleanedText.replace(/c'è/g, "c e"); // replace "ù" with "u"  
-  cleanedText = cleanedText.replace(/c'e/g, "c e"); // replace "ù" with "u"  
-  cleanedText = cleanedText.replace(/nessun'/g, "nessun "); // replace "ù" with "u"  
-  cleanedText = cleanedText.replace(/alcun'/g, "alcun "); // replace "ù" with "u"  
-  cleanedText = cleanedText.replace(/ché/g, "che"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/dà/g, "da"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/là/g, "la"); // replace "ù" with "u"
-  cleanedText = cleanedText.replace(/né/g, "ne o"); // replace "ù" with "u"  
-  cleanedText = cleanedText.replace(/sì/g, "si"); // replace "ù" with "u"  
-  cleanedText = cleanedText.replace(/tè/g, "te"); // replace "ù" with "u"  
-  cleanedText = cleanedText.replace(/ì/g, "i"); // replace "ù" with "u"  
-  cleanedText = cleanedText.replace(/Ì/g, "I"); // replace "ù" with "u"  
-  return cleanedText;
+    const replacements = {
+        "\\\\": "",  // Hinweis: "\\" wird zu "\\\\" in einem String, um den Backslash zu maskieren
+        '""': '"',
+        "\\n": "",
+        "!": ".",
+        "ä": "ae",
+        "ü": "ue",
+        "ö": "oe",
+        "Ä": "Ae",
+        "Ü": "Ue",
+        "Ö": "Oe",
+        "ß": "ss",
+        "ù": "u",
+        "à": "a",
+        "À": "A",
+        "è": "e",
+        "É": "E",
+        "é": "e",
+        "È": "E",
+        "Ú": "U",
+        "Ù": "U",
+        "ó": "o",
+        "Ó": "O",
+        "ò": "o",
+        "Ò": "O",
+        "l'u": "l u",
+        "l'a": "l a",
+        "dall'": "dall ",
+        "dell'": "dell ",
+        "all'": "all ",
+        "sull'": "sull ",
+        "nell'": "nell ",
+        "quell'": "quell ",
+        "un'a": "un a",
+        "un'u": "un u",
+        "un'o": "un o",
+        "c'è": "c e",
+        "c'e": "c e",
+        "nessun'": "nessun ",
+        "alcun'": "alcun ",
+        "ché": "che",
+        "dà": "da",
+        "là": "la",
+        "né": "ne o",
+        "sì": "si",
+        "tè": "te",
+        "ì": "i",
+        "Ì": "I"
+   };
+
+    let cleanedText = inputText;
+
+    for (let pattern in replacements) {
+        cleanedText = cleanedText.split(pattern).join(replacements[pattern]);
+    }
+
+    return cleanedText;
 }
 
 
@@ -386,7 +430,7 @@ function cleanText(inputText) {
   }
 
 function wrapText(inputText) {
-    const block = 23;
+    const block = 24;
     const words = inputText.split(' ');
     let lines = [''];
     let currentLineIndex = 0;
